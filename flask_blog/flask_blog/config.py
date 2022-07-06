665350af6d2aed0bc710bebbf8b2683986636684
vladislav <semiletoffvladislav@yandex.ru>
2022-07-06 16:41:53 +0300

    3rd commit

--- flask_blog/flask_blog/config.py
+++ flask_blog/flask_blog/config.py
@@ -3,8 +3,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
 
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = "dchitalov@gmail.com"
-    MAIL_PASSWORD = "cbcmrtyjubtD!"
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "badrhymes@yandex.ru"
+    MAIL_PASSWORD = "Ybrjkfif1"
